a34f07e95128369df63ac458dc665b61abe810d8
DanAndMeredith <DanAndMeredith@192.168.0.12>
2017-12-30 17:51:27 -0500

    Point to heroku, and don't create duplicate friends

--- src/main/java/configuration/PacemakerAPIConfiguration.java
+++ src/main/java/configuration/PacemakerAPIConfiguration.java
@@ -3,9 +3,8 @@ package configuration;
 public class PacemakerAPIConfiguration
 {
   // Local
-  public final static String PACEMAKER_API_URL = "http://localhost:7000";
+  // public final static String PACEMAKER_API_URL = "http://localhost:7000";
 
   // Heroku
-  // public final static String PACEMAKER_API_URL =
-  // "https://fathomless-springs-99040.herokuapp.com";
+  public final static String PACEMAKER_API_URL = "https://hidden-lowlands-33953.herokuapp.com";
 }
